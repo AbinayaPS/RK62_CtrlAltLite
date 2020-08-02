dfd6f65d4e0e975dbf622f83df7019547d6c112f
AbinayaPS <51849554+AbinayaPS@users.noreply.github.com>
2020-08-02 22:08:42 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
     //global variables
     action_name = "action_greet_user";
-    user_id = "jitesh97";
+    user_id = "CtrlAltLite";
 
     //if you want the bot to start the conversation
     // action_trigger();
